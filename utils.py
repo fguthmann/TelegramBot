6d0f30b544ec5c888a13e125704aec70b446efd4
fannyguthmann <fanny.guthmann@post.idc.ac.il>
2024-09-08 14:23:59 +0300

    Added string similarity to manage typo error

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import requests
+import difflib
 
 
 def get_today_date():
@@ -10,7 +11,13 @@ def generate_response(user_input: str) -> str:
     # Custom logic for response generation
     normalized_input: str = user_input.lower()
 
-    if 'what is the date' in normalized_input:
+    # List of possible valid commands
+    valid_phrases = ['what is the date', 'tell me the date', 'date today']
+
+    # Find the closest match to the user's input
+    match = difflib.get_close_matches(normalized_input, valid_phrases, n=1, cutoff=0.6)
+
+    if match:
         return get_today_date()
 
     return 'I didn’t catch that, could you please rephrase?'
